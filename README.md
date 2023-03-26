51d662e8b906d76089bca6c4d89514b17fe47206
David Toledo <dtoledoz0207@gmail.com>
2023-03-26 13:47:55 -0600

    Numbers

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Introduction to python
 https://code.tutsplus.com/courses/introduction-to-python
 
+1. [Numbers](./python-building-blocks/numbers/numbers.md)
---
+++ python-building-blocks/numbers/numbers.py
@@ -0,0 +1,31 @@
+addition = 2 + 2
+print(addition)
+
+substraction = 2 - 2
+print(substraction)
+
+multiplication = 2 * 3
+print(multiplication)
+
+divition = 10 / 2
+print(divition) # 5.0
+
+divition2 = 17 / 3
+print(divition2) # 5.666666666666667
+
+divition3 = 17 // 3
+print(divition3) # 5
+
+onlyDecimals = 17 / 3 - 17 // 3
+print(onlyDecimals) # 0.666666666666667
+
+elevation = 2 ** 3
+print(elevation) # 8
+
+print('###########################################')
+# Paying a service
+
+bill = 15.25
+tax = 7.25
+total = bill + (tax / 100) * bill
+print('total: ', total)
